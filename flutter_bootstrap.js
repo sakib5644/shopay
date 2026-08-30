968ae5a6a4037b2e2467197a5d29f6d3a77a7850
Shopay Developer <islammdsakibul140@gmail.com>
2026-08-30 16:31:01 +0600

    Update latest website version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3471737916" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "792234690" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
